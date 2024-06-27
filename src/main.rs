4dfd6f83ff7f1e7dfb601db52784841fe234bac9
Lukas Sturm <lukas@specki.its.local>
2024-06-27 05:50:57 +0200

    finalize demo

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,101 @@
 mod motorknob;
 
-use gpiod::{Chip, Bias, Options, EdgeDetect};
-use motorknob::MotorKnob;
+use gpiod::{Bias, Chip, EdgeDetect, Options, Edge};
+use motorknob::{MotorKnob, Profile};
+use serde::Deserialize;
+use std::{fs, path::Path};
+use std::process::exit;
+use std::time::Duration;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    profiles: Vec<Profile>
+}
 
 fn main() -> anyhow::Result<()> {
+    let config = load_config()?;
     let mut motor_knob = MotorKnob::new()?;
+    let mut current_profile_index = 0;
+    let mut current_profile = &config.profiles[current_profile_index];
 
-    println!("position: {}", motor_knob.get_position()?);
+    println!("Setting firsts Profile");
+    motor_knob.write_profile(current_profile)?;
 
-    // let chip = Chip::new("gpiochip0")?;
+    // libgpiod setup
+    let chip = Chip::new("gpiochip0")?;
+    let opts = Options::input([26])
+        .bias(Bias::PullUp)
+        .edge(EdgeDetect::Both)
+        .consumer("change-profile-input");
+    let mut inputs = chip.request_lines(opts)?;
 
-    // let opts = Options::input([26])
-    //     .bias(Bias::PullUp)
-    //     .edge(EdgeDetect::Falling)
-    //     .consumer("change-profile-input");
+    let mut last_edge = Edge::Rising;
+    let mut next_activation_allowed = Duration::from_millis(0);
+    loop {
+        let event = inputs.read_event()?;
 
-    // let mut inputs = chip.request_lines(opts)?;
+        // Some simple debounce logic
+        // seems to work quite ok
+        if event.edge == Edge::Rising {
+            next_activation_allowed = event.time + Duration::from_millis(100);
+            // println!("Next in {:?}", next_activation_allowed);
+        }
+        if last_edge == event.edge {
+            // println!("Skipping same edge");
+            continue;
+        }
+        last_edge = event.edge;
+        if event.time < next_activation_allowed {
+            // println!("Skipping");
+            continue;
+        }
 
-    // loop {
-    //     let event = inputs.read_event()?;
+        // Profile Switching
+        current_profile_index = (current_profile_index + 1) % config.profiles.len();
+        current_profile = &config.profiles[current_profile_index];
+        println!("Current Position {}", motor_knob.read_position()?);
+        println!("Swtichting Profile to {}", current_profile.name);
+        motor_knob.write_profile(&current_profile)?;
+        println!("Loaded Profile {:?}", motor_knob.read_current_profile()?);
+    }
+}
 
-    //     println!("event: {:?}", event);
-    // }
+static CONFIG_PATH: &str = "./Config.toml";
 
-    Ok(())
+/**
+ * Loads Config.toml or Defaults
+ */
+fn load_config() -> anyhow::Result<Config> {
+    let default_profile = Profile{
+        name: "Default".into(),
+        start_position: 60,
+        end_position: 120,
+        detents: 60,
+    };
+
+    if !Path::new(CONFIG_PATH).exists() {
+        println!("No config found. Consider creating Config.toml");
+        println!("Using default config");
+        return Ok(Config {
+            profiles: vec![default_profile]
+        })
+    }
+
+    let contents = match fs::read_to_string(CONFIG_PATH) {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("Could not open Config.toml!");
+            exit(1);
+        }
+    };
+
+    let config: Config = match toml::from_str(&contents) {
+        Ok(d) => d,
+        Err(_) => {
+            eprintln!("Unable to load config from Config.toml! Check spelling. Make sure there is atleast one Profile");
+            exit(1);
+        }
+    };
+
+    Ok(config)
 }
--- src/motorknob.rs
+++ src/motorknob.rs
@@ -1,52 +1,104 @@
+use anyhow::bail;
 use std::io::Read;
+use std::io::Write;
+use std::io::Seek;
+use std::io::SeekFrom;
+use serde::Deserialize;
 
 pub struct MotorKnob {
     position_file_handle: std::fs::File,
     end_position_file_handle: std::fs::File,
     start_position_file_handle: std::fs::File,
-    detent_position_file_handle: std::fs::File,
+    detents_file_handle: std::fs::File,
 }
 
-struct Profile {
-    name: String,
-    start_position: u16,
-    end_position: u16,
-    detent_count: u16
+// Attempt to use Typestate Pattern
+// Turned out to not be very usefull here
+// pub struct Linked;
+// pub struct Unlinked;
+
+// trait LinkState {}
+// impl LinkState for Linked {}
+// impl LinkState for Unlinked {}
+
+#[derive(Debug, Deserialize)]
+pub struct Profile {
+    pub name: String,
+    pub start_position: u16,
+    pub end_position: u16,
+    pub detents: u16,
+    // motorknob: Option<Cell<MotorKnob>>,
+    // _linked: PhantomData<LinkState>
 }
+
+// impl<L: LinkState> Profile<L> {
+//     pub fn new(name: String, start_position: u16, end_position: u16, detents: u16) ->  Profile<Unlinked> {
+//         Profile {
+//             name,
+//             start_position,
+//             end_position,
+//             detents,
+//             motorknob: None,
+//             _linked: PhantomData::<Unlinked>
+//         }
+//     }
+// }
+
+// impl Profile<Unlinked> {
+//     pub fn link(self, motorknob: Cell<MotorKnob>) -> Profile<Linked> {
+//         Profile {
+//             name: self.name,
+//             start_position: self.start_position,
+//             end_position: self.end_position,
+//             detents: self.detents,
+//             motorknob: Some(motorknob),
+//             _linked: PhantomData::<Linked>
+//         }
+//     }
+// }
+
+// impl Profile<Linked> {
+//     pub fn sync(&self) -> anyhow::Result<()> {
+//         println!("Syncing");
+
+//         Ok(())
+//     }
+// }
 
 impl MotorKnob {
     pub fn new() -> anyhow::Result<Self> {
-        let mut position_file_handle = std::fs::OpenOptions::new()
+        let position_file_handle = std::fs::OpenOptions::new()
             .read(true)
             .write(false)
             .open("/sys/motorknob/position")?;
 
-        let mut position_file_handle = std::fs::OpenOptions::new()
+        let end_position_file_handle = std::fs::OpenOptions::new()
             .read(true)
-            .write(false)
+            .write(true)
             .open("/sys/motorknob/profile/end_position")?;
 
-        let mut position_file_handle = std::fs::OpenOptions::new()
+        let start_position_file_handle = std::fs::OpenOptions::new()
             .read(true)
-            .write(false)
+            .write(true)
             .open("/sys/motorknob/profile/start_position")?;
 
-        let mut position_file_handle = std::fs::OpenOptions::new()
+        let detents_file_handle = std::fs::OpenOptions::new()
             .read(true)
-            .write(false)
-            .open("/sys/motorknob/profile/detent_position")?;
+            .write(true)
+            .open("/sys/motorknob/profile/detents")?;
 
         Ok(Self {
             position_file_handle,
             end_position_file_handle,
             start_position_file_handle,
-            detent_position_file_handle
+            detents_file_handle,
         })
     }
 
-    pub fn get_position(&mut self) -> anyhow::Result<u16> {
+    pub fn read_position(&mut self) -> anyhow::Result<u16> {
         let mut raw_position = [0u8; 2];
-        self.position_file_handle.read_exact(&mut raw_position)?;
+        self.position_file_handle.seek(SeekFrom::Start(0))?;
+        self.position_file_handle.read(&mut raw_position)?;
 
         let mut position = raw_position[0] as u16;
         position |= (raw_position[1] as u16) << 8;
@@ -54,4 +106,86 @@ impl MotorKnob {
         Ok(position)
     }
 
+    pub fn read_detents(&mut self) -> anyhow::Result<u16> {
+        let mut raw_detents = [0u8; 2];
+        self.detents_file_handle.seek(SeekFrom::Start(0))?;
+        self.detents_file_handle.read(&mut raw_detents)?;
+
+        let mut detents = raw_detents[0] as u16;
+        detents |= (raw_detents[1] as u16) << 8;
+
+        Ok(detents)
+    }
+
+    pub fn read_start_pos(&mut self) -> anyhow::Result<u16> {
+        let mut raw_start_pos = [0u8; 2];
+        self.start_position_file_handle.seek(SeekFrom::Start(0))?;
+        self.start_position_file_handle.read(&mut raw_start_pos)?;
+
+        let mut start_pos = raw_start_pos[0] as u16;
+        start_pos |= (raw_start_pos[1] as u16) << 8;
+
+        Ok(start_pos)
+    }
+
+    pub fn read_end_pos(&mut self) -> anyhow::Result<u16> {
+        let mut raw_end_pos = [0u8; 2];
+        self.end_position_file_handle.seek(SeekFrom::Start(0))?;
+        self.end_position_file_handle.read(&mut raw_end_pos)?;
+
+
+        let mut end_pos = raw_end_pos[0] as u16;
+        end_pos |= (raw_end_pos[1] as u16) << 8;
+
+        Ok(end_pos)
+    }
+
+    pub fn write_start_pos(&mut self, start_pos: u16) -> anyhow::Result<()> {
+        let count = self
+            .start_position_file_handle
+            .write(&[(start_pos >> 8) as u8, start_pos as u8])?;
+        self.start_position_file_handle.flush()?;
+        if count != 2 {
+            bail!("Start Position not fully written");
+        }
+        Ok(())
+    }
+
+    pub fn write_end_pos(&mut self, end_pos: u16) -> anyhow::Result<()> {
+        let count = self
+            .end_position_file_handle
+            .write(&[(end_pos >> 8) as u8, end_pos as u8])?;
+        self.end_position_file_handle.flush()?;
+        if count != 2 {
+            bail!("End Position not fully written");
+        }
+        Ok(())
+    }
+
+    pub fn write_detents(&mut self, detents: u16) -> anyhow::Result<()> {
+        let count = self
+            .detents_file_handle
+            .write(&[(detents >> 8) as u8, detents as u8])?;
+        self.detents_file_handle.flush()?;
+        if count != 2 {
+            bail!("Detents not fully written");
+        }
+        Ok(())
+    }
+
+    pub fn read_current_profile(&mut self) -> anyhow::Result<Profile> {
+        Ok(Profile {
+            name: "-".into(),
+            detents: self.read_detents()?,
+            end_position: self.read_end_pos()?,
+            start_position: self.read_start_pos()?,
+        })
+    }
+
+    pub fn write_profile(&mut self, profile: &Profile) -> anyhow::Result<()> {
+        self.write_start_pos(profile.start_position)?;
+        self.write_end_pos(profile.end_position)?;
+        self.write_detents(profile.detents)?;
+        Ok(())
+    }
 }
